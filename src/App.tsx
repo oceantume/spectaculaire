72d1ceb9cee749c8618f0c9679905d1dd6807fbb
Gabriel Bourgeois <gabriel.bourgeoisv4si@gmail.com>
2026-03-11 18:02:35 -0400

    Add year to title

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { ScheduleTable } from "./ScheduleTable";
 export function App() {
 	return (
 		<div class="p-4">
-			<h1 class="text-2xl font-bold mb-4">Programmation FEQ</h1>
+			<h1 class="text-2xl font-bold mb-4">Programmation FEQ 2026</h1>
 			<ScheduleTable />
 		</div>
 	);
